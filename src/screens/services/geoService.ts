4daf4d7e6fa9e8c4d2be2c4a1a36970aa38e8d88
biagmss <ars.rp9@gmail.com>
2026-05-24 16:06:33 -0300

    Bia atividade1 Geolocalização

---
+++ src/screens/services/geoService.ts
@@ -0,0 +1,21 @@
+export interface Coordenadas {
+  latitude: string;
+  longitude: string;
+}
+
+/**
+ * Analisa a URL do QR Code e extrai Latitude e Longitude usando Regex
+ */
+export const extrairCoordenadas = (url: string): Coordenadas | null => {
+  const regex = /@(-?\d+\.\d+),(-?\d+\.\d+)/;
+  const match = url.match(regex);
+
+  if (match && match[1] && match[2]) {
+    return {
+      latitude: match[1],
+      longitude: match[2],
+    };
+  }
+  
+  return null;
+};
